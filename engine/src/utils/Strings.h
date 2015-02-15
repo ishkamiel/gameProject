42663b677663777cea25c602c1f909b9a6bcae8b
Hans Liljestrand <ishkamiel@gmail.com>
2015-02-15 12:32:44 +0200

    wip

--- engine/src/utils/Strings.h
+++ engine/src/utils/Strings.h
@@ -1,44 +1,55 @@
-#ifndef UTILS_STRINGS_H
-#define UTILS_STRINGS_H
+/*
+ * Strings.h
+ *
+ *  Created on: Feb 15, 2015
+ *      Author: ishkamiel
+ */
+
+#ifndef STRINGS_H_
+#define STRINGS_H_
 
 #include <string>
 
 namespace pdUtils
 {
-    inline void formatString(std::string&) {}
+inline void formatString(std::string&)
+{
+}
 
-    template<typename T, typename... Targs>
-    inline void formatString(std::string& s, T value, Targs... args) 
-    {
-        auto pos = s.find("%") + 1;
+template<typename T, typename ... Targs>
+inline void formatString(std::string& s, T value, Targs ... args)
+{
+	auto pos = s.find("%") + 1;
 
-        while(pos < s.length()) {
-            switch (s.at(pos)) 
-            {
-                case('%'):
-                    pos = s.find("%", pos) + 1;
-                case('d'):
-                    s.replace(pos-1, 2, value);
-                    parsef(s, args...);
-                default:
-                    return;
-            }
-        }
-    }
+	while (pos < s.length())
+	{
+		switch (s.at(pos))
+		{
+		case ('%'):
+							pos = s.find("%", pos) + 1;
+		break;
+		case ('d'):
+							s.replace(pos - 1, 2, value);
+		parsef(s, args...);
+		default:
+			return;
+		}
+	}
+}
 
-    inline std::string getFormatedString(const std::string& s) 
-    {
-        std::string retval {s};
-        return s;
-    }
+inline std::string getFormatedString(const std::string& s)
+{
+	std::string retval {s};
+	return s;
+}
 
-    template<typename T, typename... Targs> 
-    inline std::string getFormatedString(const std::string& s, T a, Targs... args)
-    {
-        std::string retval {s};
-        substituteArgs(retval, a, args...);
-        return retval;
-    }
+template<typename T, typename... Targs>
+inline std::string getFormatedString(const std::string& s, T a, Targs... args)
+{
+	std::string retval {s};
+	substituteArgs(retval, a, args...);
+	return retval;
+}
 }
 
 
