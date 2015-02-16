c5bb217d7b96dc4f232f83b36b6c886e8270c5df
Hans Liljestrand <ishkamiel@gmail.com>
2015-02-16 16:58:01 +0200

    cleaned up Strings.h

--- engine/src/utils/Strings.cpp
+++ engine/src/utils/Strings.cpp
@@ -17,6 +17,7 @@ namespace pdUtils
         return trim(s, trimOption);
     }
 
+
     std::string& trim(std::string& s, Trim trimOption)
     {
         switch(trimOption) 
@@ -34,13 +35,3 @@ namespace pdUtils
         return (s);
     }
 }
-
-/*
- * Just for reference on template instantiation
-template bool operator==<char>(const pdString<char>& s, const pdString<char>& other);
-template bool operator==<char>(const pdString<char>& s, const char* other);
-template bool operator!=<char>(const pdString<char>& s, const pdString<char>& other);
-template bool operator!=<char>(const pdString<char>& s, const char* other);
-template std::ostream &operator<<<char> (std::ostream&, const pdString<char>&
-template class pdString<char>;
-*/
