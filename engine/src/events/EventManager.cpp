9193077f32ff2d295eedd8fe57525437304e8b14
Hans Liljestrand <ishkamiel@gmail.com>
2015-02-17 17:45:50 +0200

    working, working...

---
+++ engine/src/events/EventManager.cpp
@@ -0,0 +1,81 @@
+#include "EventManager.h"
+#include <vector>
+#include <memory>
+#include <list>
+#include <map>
+
+namespace pdEngine 
+{
+    class EventManagerImpl 
+    {
+        typedef std::list<EventListener> ListenerList;
+        typedef std::map<EventID, ListenerList> ListenerMap;
+
+        public:
+            EventManagerImpl();
+            ~EventManagerImpl();
+            inline EventListenerPtrW addListener(EventID, EventListener);
+            inline void removeListener(EventID, EventListenerPtrW);
+            inline void fireEvent(EventID, EventData);
+        private:
+            std::unique_ptr<ListenerMap> listenerMap;
+    };
+}
+
+namespace pdEngine
+{
+    EventManagerPtr getEventManager()
+    {
+        static EventManagerPtr em {};
+        return em;
+    }
+}
+
+namespace pdEngine
+{
+    EventManager::EventManager() : impl( new EventManagerImpl)
+    {}
+
+    EventListenerPtrW EventManager::addListener(EventID id, EventListener listener)
+    {
+        return impl->addListener(id, listener);
+    }
+
+    void EventManager::removeListener(EventID id, EventListenerPtrW listener) 
+    {
+        impl->removeListener(id, listener);
+    }
+
+    void EventManager::fireEvent(EventID id, EventData data)
+    {
+        impl->fireEvent(id, data);
+    }
+}
+
+namespace pdEngine
+{
+    EventManagerImpl::EventManagerImpl() : listenerMap(new ListenerMap)
+    {}
+
+    EventListenerPtrW EventManagerImpl::addListener(EventID id, EventListener listener)
+    {
+        // std::map::operator[] does insertion if not existing
+        listenerMap->operator[](id).push_back(listener);
+    }
+
+    void EventManagerImpl::removeListener(EventID id, EventListenerPtrW listener)
+    {
+        return true;
+    }
+
+    void EventManagerImpl::fireEvent(EventID id, EventData data)
+    {
+         auto search = listenerMap->find(id);
+         if(search != listenerMap->end()) {
+             for (auto l : search->second)
+                 l(data);
+         }
+    }
+}
+
+
---
+++ engine/src/events/EventManager.h
@@ -0,0 +1,52 @@
+#ifndef EVENTMANAGER_H_
+#define EVENTMANAGER_H_
+
+#include <memory>
+#include <string>
+#include <functional>
+
+namespace pdEngine
+{
+    class EventManager;
+    class EventManagerImpl;
+    class EventListenerHandle;
+
+    typedef std::string EventData;
+    typedef std::shared_ptr<EventManager> EventManagerPtr;
+    typedef std::function<void(EventData)> EventListener;
+    typedef int EventID;
+    typedef int ListenerID;
+    typedef std::shared_ptr<EventListenerHandle> EventListenerPtrS;
+    typedef std::weak_ptr<EventListenerHandle> EventListenerPtrW;
+
+    EventManagerPtr getEventManager();
+
+    class EventListenerHandle
+    {
+        public:
+            ~EventListenerHandle();
+            void cancel();
+        private:
+            EventListenerHandle(EventListener);
+            inline void fire();
+            inline ListenerID listenerID();
+            inline EventID eventID();
+            inline bool isRemoved();
+    };
+
+    class EventManager
+    {
+        public:
+            EventListenerPtrW addListener(EventID, EventListener);
+            void removeListener(EventID, EventListenerPtrW);
+            void fireEvent(EventID, EventData);
+
+        private:
+            std::unique_ptr<EventManagerImpl> impl;
+            EventManager();
+            ~EventManager();
+    };
+}
+
+#endif
+
