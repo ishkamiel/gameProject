e52e576d6216336591510eecf4f911f168517525
Hans Liljestrand <ishkamiel@gmail.com>
2015-02-27 12:27:44 +0200

    wip

--- engine/includes/EventManager.h
+++ engine/includes/EventManager.h
@@ -17,29 +17,29 @@ namespace pdEngine
 	
 	typedef std::string EventData;
 	typedef std::function<bool(EventData)> EventListenerDelegate;
-	typedef std::vector<EventID> EventList;
 	typedef std::shared_ptr<EventManager> EventManagerSharedPtr;
 	typedef std::unique_ptr<EventListener> EventListenerUniquePtr;
-	
-	
+
 	class EventListener
 	{
-		friend class EventManager;
+		friend class EventManage;
+
+		EventID eventID;
+		EventListenerDelegate& delegate;
+		EventManagerSharedPtr eventManager;
 
 	public:
-		EventList getEventList() const;
-		void cancelAll();
-		void canel(EventID);
+		const EventID getEventID() const;
+		void cancel();
 
 	private:
-		EventListener(EventID, EventListenerDelegate);
-		EventListener(EventList, EventListenerDelegate);
+		EventListener();
+		EventListener(EventManagerSharedPtr, const EventID, EventListenerDelegate&);
 		~EventListener();
 	};
 	
 	class EventManager : public Task
 	{
-		friend EventManagerSharedPtr getEventManager();
 		friend class EventListener;
 
 		typedef std::shared_ptr<std::vector<EventListenerDelegate>> EventListenerList;
@@ -51,9 +51,8 @@ namespace pdEngine
 		EventManager();
 		~EventManager();
 
-		int fireEvent(EventID, EventData);
-		EventListenerUniquePtr createEventListener(EventID, EventListenerDelegate);
-		EventListenerUniquePtr createEventListener(EventList, EventListenerDelegate);
+		int fireEvent(const EventID, const EventData&);
+		EventListenerUniquePtr createEventListener(const EventID, EventListenerDelegate&);
 		
 	private:
 		void addListener(const EventID, const EventListenerDelegate&);
--- engine/src/EventManager.cpp
+++ engine/src/EventManager.cpp
@@ -1,44 +1,91 @@
 #include "EventManager.h"
 
+#include <assert.h>
+
 namespace pdEngine
 {
 	EventManager::EventManager()
 	: eventListeners()
 	{}
 
-	int EventManager::fireEvent(EventID id, EventData eventData)
+	int EventManager::fireEvent(const EventID& id, const EventData& eventData)
 	{
-		int listenersCalled = 0;
 		auto list = eventListeners.find(id);
 
-		if (list != eventListeners.end())
+		if (list == eventListeners.end())
+			return(0); // no registered listeners for event
+
+		auto listenersCalled = 0;
+		for (auto listener = list->second->begin(); listener != list->second->end(); list++)
 		{
-			for (auto listener = list->second->begin(); listener != list->second->end(); list++)
+			if (! (*listener)(eventData))
 			{
-				if (! listener(eventData)) //FIXME
-				{
-					return(++listenersCalled);
-				}
-				++listenersCalled;
+				return(++listenersCalled);
 			}
+			++listenersCalled;
 		}
 
 		return(listenersCalled);
 	}
 
-	EventListenerUniquePtr EventManager::createEventListener(EventID, EventListenerDelegate)
+	EventListenerUniquePtr EventManager::createEventListener(EventListenerDelegate& eld)
 	{
 	}
 
-	EventListenerUniquePtr EventManager::createEventListener(EventList, EventListenerDelegate)
+	EventListenerUniquePtr EventManager::createEventListener(EventListenerDelegate& eld, EventID& eventID)
 	{
 	}
 
-	void EventManager::addListener(const EventID, const EventListenerDelegate&)
+	void EventManager::addListener(const EventID id, const EventListenerDelegate& delegate)
 	{
+		auto list = eventListeners[id];
+		list->push_back(delegate);
 	}
 
-	void EventManager::removeListener(const EventID, const EventListenerDelegate&)
+	void EventManager::removeListener(const EventID id, const EventListenerDelegate& delegate)
 	{
+		auto foundList = eventListeners.find(id);
+
+		if (foundList == eventListeners.end())
+			return;
+
+		auto foundDelegate = foundList->second->find(delegate);
+
+		if (foundDelegate == foundDelegate->second->end())
+			return; // hmm, cannot find delegate
+
+		foundList->second->erase(foundDelegate);
+	}
+
+	/*
+	 * EventListener
+	 */
+
+	EventListener::EventListener(EventManager& em, EventID& id, EventListenerDelegate delegate)
+	: eventManager(em), eventID(id), delegate(delegate)
+	{
+		assert(eventManager != nullptr && id != nullptr & delegate != nullptr);
+		eventManager->addListener(id, delegate);
+	}
+
+	EventListener::~EventListener()
+	{
+		cancel();
+	}
+
+	void EventListener::cancel()
+	{
+		if (eventManager != nullptr)
+		{
+			eventManager->removeListener(eventID, delegate);
+			eventManager.reset();
+			// FIXME need to fix this once FastDelegate stuff is added.
+			//delete delegate;
+		}
+	}
+
+	const EventID EventListener::getEventID() const
+	{
+		return(eventID);
 	}
 }
