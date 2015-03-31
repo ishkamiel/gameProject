075f454f2e6e50bf94aec2ed1d4c85c4cdfe686d
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-31 19:04:36 +0300

    using ninja to get gmock doesn't seem to work on vivid

--- cmake/GetGmock.cmake
+++ cmake/GetGmock.cmake
@@ -1,6 +1,5 @@
 # We need thread support
 find_package(Threads REQUIRED)
-
 include(ExternalProject)
 
 ExternalProject_Add(gtest
@@ -12,12 +11,14 @@ add_library(libgtest IMPORTED STATIC GLOBAL)
 add_dependencies(libgtest gtest)
 
 ExternalProject_Get_Property(gtest SOURCE_DIR BINARY_DIR)
+file(MAKE_DIRECTORY "${SOURCE_DIR}/include")
 set_target_properties(libgtest PROPERTIES
     "IMPORTED_LOCATION" "${BINARY_DIR}/libgtest.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
     "INTERFACE_INCLUDE_DIRECTORIES" "${SOURCE_DIR}/include"
     )
 # include_directories("${SOURCE_DIR}/include")
+
 
 ExternalProject_Add(gmock
     URL https://googlemock.googlecode.com/files/gmock-1.7.0.zip
@@ -30,6 +31,7 @@ add_dependencies(libgmock gmock)
 
 # Set gmock properties
 ExternalProject_Get_Property(gmock SOURCE_DIR BINARY_DIR)
+file(MAKE_DIRECTORY "${SOURCE_DIR}/include")
 set_target_properties(libgmock PROPERTIES
     "IMPORTED_LOCATION" "${BINARY_DIR}/libgmock.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
