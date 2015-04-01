06f4b46deaa9349e255221d59ee3a5f4facb05f3
Hans Liljestrand <ishkamiel@gmail.com>
2015-04-01 16:24:20 +0300

    added VARIABLE_IS_NOT_USED macro

--- engine/include/Utils.h
+++ engine/include/Utils.h
@@ -40,4 +40,10 @@ namespace pdEngine
 #define DLOG( ... )
 #endif /* NDEBUG */
 
+#ifdef __GNUC__
+#define VARIABLE_IS_NOT_USED __attribute__ ((unused))
+#else
+#define VARIABLE_IS_NOT_USED
+#endif
+
 #endif /* PDENGINE_UTILS_H_ */
