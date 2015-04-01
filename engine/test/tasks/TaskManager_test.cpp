4bd7ad8ee459158aa8b4f7bee5ce0fb76a3fda40
Hans Liljestrand <ishkamiel@gmail.com>
2015-04-01 20:51:14 +0300

    Kinda done

--- engine/test/tasks/TaskManager_test.cpp
+++ engine/test/tasks/TaskManager_test.cpp
@@ -29,11 +29,11 @@ protected:
     {
         auto t = std::make_shared<pdEngine::MockTask>();
 
-        EXPECT_CALL(*t1, onInit()).Times(1);
-        EXPECT_CALL(*t1, onUpdate(_)).Times(AtLeast(1));
-        EXPECT_CALL(*t1, onSuccess()).Times(0);
-        EXPECT_CALL(*t1, onAbort()).Times(0);
-        EXPECT_CALL(*t1, onFail()).Times(1);
+        EXPECT_CALL(*t, onInit()).Times(1);
+        EXPECT_CALL(*t, onUpdate(_)).Times(AtLeast(1));
+        EXPECT_CALL(*t, onSuccess()).Times(0);
+        EXPECT_CALL(*t, onAbort()).Times(0);
+        EXPECT_CALL(*t, onFail()).Times(1);
 
         return t;
     }
@@ -42,11 +42,11 @@ protected:
     {
         auto t = std::make_shared<pdEngine::MockTask>();
 
-        EXPECT_CALL(*t1, onInit()).Times(1);
-        EXPECT_CALL(*t1, onUpdate(_)).Times(AtLeast(1));
-        EXPECT_CALL(*t1, onSuccess()).Times(1);
-        EXPECT_CALL(*t1, onAbort()).Times(0);
-        EXPECT_CALL(*t1, onFail()).Times(0);
+        EXPECT_CALL(*t, onInit()).Times(1);
+        EXPECT_CALL(*t, onUpdate(_)).Times(AtLeast(1));
+        EXPECT_CALL(*t, onSuccess()).Times(1);
+        EXPECT_CALL(*t, onAbort()).Times(0);
+        EXPECT_CALL(*t, onFail()).Times(0);
 
         return t;
     }
@@ -55,11 +55,11 @@ protected:
     {
         auto t = std::make_shared<pdEngine::MockTask>();
 
-        EXPECT_CALL(*t1, onInit()).Times(1);
-        EXPECT_CALL(*t1, onUpdate(_)).Times(AtLeast(1));
-        EXPECT_CALL(*t1, onSuccess()).Times(0);
-        EXPECT_CALL(*t1, onAbort()).Times(0);
-        EXPECT_CALL(*t1, onFail()).Times(0);
+        EXPECT_CALL(*t, onInit()).Times(1);
+        EXPECT_CALL(*t, onUpdate(_)).Times(AtLeast(1));
+        EXPECT_CALL(*t, onSuccess()).Times(0);
+        EXPECT_CALL(*t, onAbort()).Times(0);
+        EXPECT_CALL(*t, onFail()).Times(0);
 
         return t;
     }
@@ -75,6 +75,18 @@ void TaskManager_test::SetUp()
     EXPECT_CALL(*t2, onInit()).Times(AtMost(1));
     EXPECT_CALL(*t3, onInit()).Times(AtMost(1));
     EXPECT_CALL(*t4, onInit()).Times(AtMost(1));
+    EXPECT_CALL(*t1, onFail()).Times(AtMost(1));
+    EXPECT_CALL(*t2, onFail()).Times(AtMost(1));
+    EXPECT_CALL(*t3, onFail()).Times(AtMost(1));
+    EXPECT_CALL(*t4, onFail()).Times(AtMost(1));
+    EXPECT_CALL(*t1, onAbort()).Times(AtMost(1));
+    EXPECT_CALL(*t2, onAbort()).Times(AtMost(1));
+    EXPECT_CALL(*t3, onAbort()).Times(AtMost(1));
+    EXPECT_CALL(*t4, onAbort()).Times(AtMost(1));
+    EXPECT_CALL(*t1, onSuccess()).Times(AtMost(1));
+    EXPECT_CALL(*t2, onSuccess()).Times(AtMost(1));
+    EXPECT_CALL(*t3, onSuccess()).Times(AtMost(1));
+    EXPECT_CALL(*t4, onSuccess()).Times(AtMost(1));
     tm.reset(new pdEngine::TaskManager());
 }
 
@@ -292,8 +304,8 @@ TEST_F(TaskManager_test, AbortAllWithMoreTasks)
 
 TEST_F(TaskManager_test, AlotWithPause)
 {
-    auto tasks = 100;
-    auto iterations  = 100000;
+    auto tasks = 10;
+    auto iterations  = 10;
 
     auto f = getFailing();
     auto s = getSucceeding();
@@ -304,15 +316,23 @@ TEST_F(TaskManager_test, AlotWithPause)
     EXPECT_CALL(*t1, onAbort()).Times(0);
     EXPECT_CALL(*t1, onFail()).Times(0);
     tm->addTask(t1);
+    tm->updateTasks(1);
+    ASSERT_EQ(t1->getState(), pdEngine::TaskState::ready);
+    tm->updateTasks(1);
+    ASSERT_EQ(t1->getState(), pdEngine::TaskState::running);
 
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
     for (auto i = 0; i < tasks; ++i) tm->addTask(getEndless());
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
+
     tm->addTask(f);
+
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
     for (auto i = 0; i < tasks; ++i) tm->addTask(getEndless());
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
+
     tm->addTask(s);
+
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
     for (auto i = 0; i < tasks; ++i) tm->addTask(getEndless());
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
@@ -322,28 +342,58 @@ TEST_F(TaskManager_test, AlotWithPause)
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
     EXPECT_EQ(tm->taskCount(), taskCount-1);
 
-    EXPECT_CALL(*f, onUpdate(_)).WillOnce(InvokeWithoutArgs(&*f, &pdEngine::Task::succeed));
+    EXPECT_CALL(*s, onUpdate(_)).WillOnce(InvokeWithoutArgs(&*s, &pdEngine::Task::succeed));
     for (auto i = 0; i < iterations; ++i) tm->updateTasks(1);
     EXPECT_EQ(tm->taskCount(), taskCount-2);
 }
 
-// TEST_F(TaskManager_test, AnyDead)
-// {
-//     t1->failOnInit = true;
-//     tm->addTask(t1);
-//     tm->addTask(t2);
-//     tm->addTask(t3);
-//     tm->addTask(t4);
-//     tm->initAll();
-//     ASSERT_TRUE(tm->areAnyDead());
-// }
+TEST_F(TaskManager_test, AnyDeadHasNoFalsePositives)
+{
+    tm->addTask(t1);
+    tm->addTask(t2);
+    tm->addTask(t3);
+    tm->addTask(t4);
 
+    EXPECT_CALL(*t1, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t2, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t3, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t4, onUpdate(_)).Times(AtLeast(1));
 
-// bool initAll();
-// void abortAllNow();
-// void addTask(Task_sptr);
-// void updateTasks(TimeDelta);
-// void pauseTasks();
-// void unPauseTasks();
-// bool areAnyDead();
+    ASSERT_FALSE(tm->areAnyDead());
+    tm->initAll();
+    ASSERT_FALSE(tm->areAnyDead());
+    tm->updateTasks(1);
+    ASSERT_FALSE(tm->areAnyDead());
+    tm->updateTasks(1);
+    ASSERT_FALSE(tm->areAnyDead());
+    tm->updateTasks(1);
+    ASSERT_FALSE(tm->areAnyDead());
+}
 
+TEST_F(TaskManager_test, AnyDeadNoticesDeadAtInit)
+{
+    auto t = std::make_shared<pdEngine::MockTask>();
+
+    EXPECT_CALL(*t, onInit()).Times(1).WillOnce(InvokeWithoutArgs(&*t, &pdEngine::Task::fail));
+    EXPECT_CALL(*t, onUpdate(_)).Times(0);
+    EXPECT_CALL(*t, onSuccess()).Times(0);
+    EXPECT_CALL(*t, onAbort()).Times(0);
+    EXPECT_CALL(*t, onFail()).Times(1);
+
+    EXPECT_CALL(*t1, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t2, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t3, onUpdate(_)).Times(AtLeast(1));
+    EXPECT_CALL(*t4, onUpdate(_)).Times(AtLeast(1));
+
+    tm->addTask(t);
+    tm->addTask(t1);
+    tm->addTask(t2);
+    tm->addTask(t3);
+    tm->addTask(t4);
+    tm->initAll();
+    ASSERT_TRUE(tm->areAnyDead());
+    tm->updateTasks(1);
+    tm->updateTasks(1);
+    tm->updateTasks(1);
+    tm->updateTasks(1);
+}
