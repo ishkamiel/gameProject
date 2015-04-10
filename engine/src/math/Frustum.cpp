3dcc611a37d5bb7487dddb969f9f128cdb004020
Hans Liljestrand <ishkamiel@gmail.com>
2015-04-10 15:31:05 +0300

    Frustom cheks with radius

--- engine/src/math/Frustum.cpp
+++ engine/src/math/Frustum.cpp
@@ -28,29 +28,27 @@ Frustum::~Frustum()
 
 void Frustum::init(void) noexcept
 {
-    PD_debug("Using a fov: {}", m_Fov);
-    PD_debug("Using a apsect ratio: {}", m_Aspect);
+    //float z_dir = (m_Near-m_Far) / std::abs(m_Near-m_Far);
 
-    float z_dir = (m_Near-m_Far) / std::abs(m_Near-m_Far);
+	float diff = m_Far - m_Near;
+	float depth_dir = diff/ std::abs(diff);
 
     float x = cos((m_Fov/m_Aspect)/2.0f);
-    float zx = sin((m_Fov/m_Aspect)/2.0f) * z_dir;
+    float hz = (-depth_dir) * sin((m_Fov/m_Aspect)/2.0f);
     float y = cos(m_Fov/2.0f);
-    float zy = sin(m_Fov/2.0f) * z_dir;
+    float vz = (-depth_dir) * sin(m_Fov/2.0f);
 
-    PD_debug("{}/{}={}", m_Fov, m_Aspect, m_Fov/m_Aspect);
-    PD_debug("acos({}/{})={}", m_Fov, m_Aspect, acos(m_Fov/m_Aspect));
-
-    assert((m_Near != 0.0f || m_Far != 0.0f) && "cannot compute normals");
-    assert((x == x && y==y && zx==zx && zy==zy) && "something is NaN");
+    assert((m_Near != 0.0f || m_Far != 0.0f) && "zero depth frustum");
+	assert(std::abs(m_Far) > std::abs(m_Near) && "bad Far and Near clips");
+    assert((x == x && y==y && hz==hz && vz==vz) && "something is NaN");
 
     m_Planes.reserve(6);
-    m_Planes.emplace_back(0, 0, z_dir, std::abs(m_Near));
-    m_Planes.emplace_back(0, 0, -z_dir, std::abs(m_Far));
-    m_Planes.emplace_back(0.0f, y, zy, 0.0f); // upper
-    m_Planes.emplace_back(0.0f, -y, zy, 0.0f); // lower
-    m_Planes.emplace_back(x, 0.0f, zx, 0.0f); // sides
-    m_Planes.emplace_back(-x, 0.0f, zx, 0.0f);
+    m_Planes.emplace_back(0, 0, -depth_dir, m_Near);
+    m_Planes.emplace_back(0, 0, depth_dir, std::abs(m_Far));
+    m_Planes.emplace_back(0.0f, y, vz, 0.0f); // upper
+    m_Planes.emplace_back(0.0f, -y, vz, 0.0f); // lower
+    m_Planes.emplace_back(x, 0.0f, hz, 0.0f); // sides
+    m_Planes.emplace_back(-x, 0.0f, hz, 0.0f);
 }
 
 std::ostream& operator<<(std::ostream& os, const Frustum& f) noexcept
--- engine/test/math/Frustum_test.cpp
+++ engine/test/math/Frustum_test.cpp
@@ -26,9 +26,11 @@ class Frustum_test : public ::testing::Test
     };
 
 
-TEST_F(Frustum_test, TestEasyFrustum)
+TEST_F(Frustum_test, IsInsideEasyFrustum)
 {
     Frustum f { 3, 1, 0.0f, 100.0f };
+    PD_debug("Frustum is: {}", f);
+
     ASSERT_TRUE(f.isInside(Vector3(0,0,10)));
 
     ASSERT_FALSE(f.isInside(Vector3(0,0,-10)));
@@ -38,14 +40,14 @@ TEST_F(Frustum_test, TestEasyFrustum)
     ASSERT_FALSE(f.isInside(Vector3(0,-10,0)));
 }
 
-TEST_F(Frustum_test, TestDefaultFrustum)
+TEST_F(Frustum_test, DefaultFrustum)
 {
     Frustum f {};
     ASSERT_TRUE(f.isInside(Vector3(0,0,10)));
     ASSERT_FALSE(f.isInside(Vector3(0,0,-10)));
 }
 
-TEST_F(Frustum_test, TestFrustumWithNegativeZ)
+TEST_F(Frustum_test, ISInsiderustumWithNegativeZ)
 {
     Frustum f { PI/2.0f, 1, -10.0f, -100.0f };
 
@@ -55,7 +57,27 @@ TEST_F(Frustum_test, TestFrustumWithNegativeZ)
     ASSERT_TRUE(f.isInside(Vector3(1,1,-20)));
     ASSERT_TRUE(f.isInside(Vector3(-5,-5,-50)));
 
+    ASSERT_FALSE(f.isInside(Vector3(0,0,-9)));
+    ASSERT_FALSE(f.isInside(Vector3(0,0,-101)));
     ASSERT_FALSE(f.isInside(Vector3(-50,-50,-20)));
+}
+
+TEST_F(Frustum_test, IsInsideWithRadius)
+{
+	auto fov = PI/2.0f;
+    Frustum f { fov, 1, -10.0f, -100.0f };
+
+    ASSERT_TRUE(f.isInside(Vector3(0,0,-11), 1.0f));
+    ASSERT_FALSE(f.isInside(Vector3(0,0,1000), 1.0f));
+
+    ASSERT_TRUE(f.isInside(Vector3(0,0,0), 100.0f));
+    ASSERT_FALSE(f.isInside(Vector3(0,0,0), 9.01f));
+
+    ASSERT_TRUE(f.isInside(Vector3(0,0,-101), 1.1f));
+    ASSERT_FALSE(f.isInside(Vector3(0,0,-101), 0.99f));
+
+    ASSERT_TRUE(f.isInside(Vector3(0,100*tan(fov/2.0f)+0.5f,-100), 1.0f));
+    ASSERT_FALSE(f.isInside(Vector3(0,100*tan(fov/2.0f+0.5f),-100), 0.1f));
 }
 
 
