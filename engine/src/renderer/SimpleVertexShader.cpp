0eccc97d693ac4b74188457c646e1414153bad22
Hans Liljestrand <ishkamiel@gmail.com>
2015-04-12 12:21:39 +0300

    wip

--- engine/src/renderer/SimpleVertexShader.cpp
+++ engine/src/renderer/SimpleVertexShader.cpp
@@ -15,7 +15,7 @@ SimpleVertexShader::~SimpleVertexShader() {
 
 bool SimpleVertexShader::compile(void) {
     return compileBuffer(GL_VERTEX_SHADER,{
-        "#version 140\n"
+        "#version 330\n"
         "in vec2 LVertexPos2D;\n"
         "void main() {\n"
         "	gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 );\n"
