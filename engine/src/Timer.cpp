0dd57ba659ab56adecfed27c99b46b697b027a1a
Hans Liljestrand <ishkamiel@gmail.com>
2015-04-23 18:38:32 +0300

    Timer fixin

--- engine/src/Timer.cpp
+++ engine/src/Timer.cpp
@@ -13,51 +13,29 @@ namespace pdEngine
 Timer::Timer(int freq)
     : start(std::chrono::high_resolution_clock::now()), prev(start)
 {
-    std::chrono::seconds oneSec {1};
-    timeSlice = std::chrono::duration_cast<Duration>(oneSec);
+    m_delta = 1000 / freq;
+    timeSlice = std::chrono::duration_cast<Duration>(std::chrono::milliseconds(m_delta));
 
-    timeSlice = (timeSlice/freq);
-    /*
-    rest = timeSlice - std::chrono::duration_cast<Duration>(
-        std::chrono::milliseconds(getStepDeltaMs()));
-	*/
+    if (m_delta * freq != 1000) {
+        PDE_WARN << "1000ms not divisible by given frequency " << freq;
+    }
 
     PDE_DEBUG << "Updates per second set to " << freq << ", milliseconds per update: " << timeSlice.count();
 }
 
-int Timer::getStepDeltaMs(void) const noexcept
-{
-	 return std::chrono::duration_cast<std::chrono::milliseconds>(timeSlice).count();
-}
-
-
-bool Timer::stepAndSleep(void)
-{
-    //auto sleepTime = timeSlice - (now() - prev);
-    //if (sleepTime.count() > 0)
-        //std::this_thread::sleep_for(sleepTime);
-//
-    //delta = now()-prev;
-    //prev += delta;
-
-    //return delta.count();
-    //return std::chrono::duration_cast<std::chrono::microseconds>(delta).count();
-    return false;
-}
-
 unsigned long Timer::totalMicro(void) const
 {
-    return std::chrono::duration_cast<std::chrono::microseconds>(prev-start).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(now() - start).count();
 }
 
 unsigned long Timer::totalMilli(void) const
 {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(prev-start).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(now() - start).count();
 }
 
 unsigned long Timer::totalSeconds(void) const
 {
-    return (std::chrono::duration_cast<std::chrono::seconds>(prev-start)).count();
+    return (std::chrono::duration_cast<std::chrono::seconds>(now() - start)).count();
 }
 
 }
--- engine/src/Timer.h
+++ engine/src/Timer.h
@@ -10,26 +10,31 @@ class Timer
     using TimePoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
     using Duration = std::chrono::high_resolution_clock::duration;
 
+    TimePoint start;
+    TimePoint prev;
+    Duration timeSlice;
+    int m_delta;
+
 public:
     static inline TimePoint now() noexcept;
 
-public:
     Timer(const int stepsPerSecond);
     virtual ~Timer() = default;
 
-    bool stepAndSleep(void);
-
+    /**
+     * @brief Checks whether it's time to execute another step.
+     */
     inline bool step(void) noexcept;
-    int getStepDeltaMs(void) const noexcept;
+
+    /**
+     * @brief Retrieves the delta time for each time step. Provided the timer frequency is
+     * unaltered this will always be the same value.
+     */
+    inline int getStepDeltaMs(void) const noexcept;
 
     unsigned long totalMicro(void) const;
     unsigned long totalMilli(void) const;
     unsigned long totalSeconds(void) const;
-
-private:
-    TimePoint start;
-    TimePoint prev;
-    Duration timeSlice;
 };
 
 /*
@@ -48,6 +53,13 @@ bool Timer::step(void) noexcept
     prev+=timeSlice;
     return true;
 }
+
+int Timer::getStepDeltaMs(void) const noexcept
+{
+    return m_delta;
+}
+
+
 
 }
 
